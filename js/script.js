6a1c9a3966bb9a004c796ef8e96b27d06a45a4a5
Liu, Zizuo <ziliu@paypal.com>
2014-11-02 16:53:19 -0800

    for script animate class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+
   $(".myh1").click(function(){
-    $("h2").hide();
+    $("h2").hide(function(){
+    	console.log(this);
+    });
   });
+
+
+  $(".my1a").click(function(){
+  	$("#myul").hide();
+  	$("h2").addClass("animate");
+  });
+
 });
+
